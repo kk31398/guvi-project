683772d9e1f17830d0363d7348d8a066de1fd560
kk31398 <38251581+kk31398@users.noreply.github.com>
2018-06-05 09:46:58 +0530

    Create check_armstrong_btwn_interval.py

---
+++ check_armstrong_btwn_interval.py
@@ -0,0 +1,12 @@
+x,y=[int(a) for a in input().split()]
+for num in range(x,y+1):
+
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum +=pow(digit,3)
+        temp //= 10
+
+    if num == sum:
+        print(num,end=' ')
